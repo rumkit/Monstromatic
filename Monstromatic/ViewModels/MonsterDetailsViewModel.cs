f575639c84676eb61dbb78dd06920d8ad937348b
Anton Mitsengendler <a.mic@rtc.ru>
2020-07-17 23:49:07 +0300

    finished details window draft design
    added fody package
    updated detailsVM with reactive properties

--- Monstromatic/ViewModels/MonsterDetailsViewModel.cs
+++ Monstromatic/ViewModels/MonsterDetailsViewModel.cs
@@ -1,17 +1,63 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Threading.Channels;
+using Avalonia.Controls.Templates;
+using DynamicData.Binding;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using Console = System.Console;
 
 namespace Monstromatic.ViewModels
 {
-    public class MonsterDetailsViewModel
+    public class MonsterDetailsViewModel : ReactiveObject
     {
+        [Reactive]
         public string Name { get; set; }
 
-        public int Level { get; set; }
+        private int _level;
+
+        public int Level
+        {
+            get => _level + GetLevelModifier();
+            set => this.RaiseAndSetIfChanged(ref _level, value);
+        }
 
         public List<string> Features { get; set; }
 
-        public int Moral => Level;
+        public int Moral { [ObservableAsProperty] get; }
 
-        public int Bravery => Level;
+        public int Bravery { [ObservableAsProperty] get; }
+
+        [Reactive]
+        public bool HasAdvantage { get; set; }
+
+        [Reactive]
+        public bool HasDisadvantage { get; set; }
+
+        public MonsterDetailsViewModel()
+        {
+            this.WhenAnyValue(x => x.HasAdvantage).Subscribe(x =>
+            {
+                if (x)
+                    HasDisadvantage = false;
+                this.RaisePropertyChanged(nameof(Level));
+            });
+
+            this.WhenAnyValue(x => x.HasDisadvantage).Subscribe(x =>
+            {
+                if (x)
+                    HasAdvantage = false;
+                this.RaisePropertyChanged(nameof(Level));
+            });
+
+            this.WhenAnyValue(x => x.Level).ToPropertyEx(this, x => x.Moral);
+            this.WhenAnyValue(x => x.Level).ToPropertyEx(this, x => x.Bravery);
+        }
+
+        private int GetLevelModifier()
+        {
+            return (HasAdvantage ? 1 : 0) - (HasDisadvantage ? 1 : 0);
+        }
     }
 }
