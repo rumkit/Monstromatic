650b75a08083b2e8c232e01ba62b7462dbaa76d3
Anton Mitsengendler <a.mic@rtc.ru>
2020-11-13 07:05:17 +0300

    counter control proto

---
+++ Monstromatic/Views/HitCounter.axaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Monstromatic.Views
+{
+    public class HitCounter : UserControl
+    {
+        public HitCounter()
+        {
+            this.InitializeComponent();
+        }
+
+        public static readonly StyledProperty<int> CounterSourceProperty =
+            AvaloniaProperty.Register<HitCounter, int>(nameof(CounterSource));
+
+        public int CounterSource
+        {
+            get => GetValue(CounterSourceProperty);
+            set => SetValue(CounterSourceProperty, value);
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
