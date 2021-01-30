ae5d0ea4423ee651e11365d202f23bcc2115c909
Anton Mitsengendler <a.mic@rtc.ru>
2021-01-30 04:38:01 +0300

    updated travis config
    csproj version is set to both system and user vars

--- .travis/get-project-version.ps1
+++ .travis/get-project-version.ps1
@@ -1,3 +1,4 @@
 $proj = [xml](Get-Content .\Monstromatic\Monstromatic.csproj)
 $version = $proj.Project.PropertyGroup.VersionPrefix
-[System.Environment]::SetEnvironmentVariable('csproj_version',"$version",[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("csproj_version","$version",[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("csproj_version","$version",[System.EnvironmentVariableTarget]::User)
